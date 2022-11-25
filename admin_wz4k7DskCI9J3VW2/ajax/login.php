87e025efeabdaef6c50062495e002e8901655bee
ndua1994 <duanishant71@gmail.com>
2022-11-25 19:58:00 +0530

    dashboard page setup and session setup

--- admin_wz4k7DskCI9J3VW2/ajax/login.php
+++ admin_wz4k7DskCI9J3VW2/ajax/login.php
@@ -9,11 +9,16 @@ $password=$_POST['password'];
 
 $query=mysqli_query($conn,"select * from tbl_login where email_id='$email_id' and password='".md5($password)."' and is_active=1");
 
+$record=mysqli_fetch_array($query);
+$login_user=mysqli_fetch_array(mysqli_query($conn,"select * from tbl_login_type where login_type_id=".$record['login_type'].""));
+
 $count=mysqli_num_rows($query);
 
 if($count==1)
 {
-	$_SESSION['email_id']=$email_id;
+	$_SESSION['email_id']=$record['email_id'];
+	$_SESSION['username']=$record['username'];
+	$_SESSION['designation']=$login_user['login_type_name'];
 	$attr=array('status'=>'success','msg'=>'Login Successfull !');
 }
 else
